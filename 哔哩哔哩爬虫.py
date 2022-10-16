d18b706ae3f937d3ab1b7b17673050ed2030e95d
Calccx <allcalc@outlook.com>
2022-10-16 19:21:26 +0800

    实现了粉丝、关注等数据的追加

--- 哔哩哔哩爬虫.py
+++ 哔哩哔哩爬虫.py
@@ -11,25 +11,27 @@ import math
 uid=input()
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.42'}
 
+url='https://api.bilibili.com/x/relation/followings?vmid='+uid+'&pn=1'
+page_data=requests.get(url=url,headers=headers).text
+names='"mid":(.*?),".*?"uname":"(.*?)"'
 
-def getNames(uid):
-    url='https://api.bilibili.com/x/relation/followings?vmid='+uid+'&pn=1'
-    page_data=requests.get(url=url,headers=headers).text
-    names='"mid":(.*?),".*?"uname":"(.*?)"'
-    
-    names_data=re.findall(names,page_data,re.S)
-    
-    follow_json=json.loads(page_data)
-    total=follow_json['data']['total']
-    if total>250:
-        total=250
-    pn=math.ceil(total/50)
-    for i in range(2,pn+1):
-        url_follow=url='https://api.bilibili.com/x/relation/followings?vmid='+uid+'&pn='+str(i)
-        page_data_follow=requests.get(url=url_follow,headers=headers).text
-        names_data.extend(re.findall(names,page_data_follow,re.S))
-    return names_data,total
-names_data,total=getNames(uid)
-for name in names_data:
-    print(name)
-print(len(names_data))
+names_data=re.findall(names,page_data,re.S)
+
+follow_json=json.loads(page_data)
+total=follow_json['data']['total']
+if total>250:
+    total=250
+pn=math.ceil(total/50)
+for i in range(2,pn+1):
+    url_follow=url='https://api.bilibili.com/x/relation/followings?vmid='+uid+'&pn='+str(i)
+    page_data_follow=requests.get(url=url_follow,headers=headers).text
+    names_data.extend(re.findall(names,page_data_follow,re.S))
+
+def getRelations(userId):
+    relation_text_ff=requests.get("https://api.bilibili.com/x/relation/stat?vmid={}&jsonp=jsonp".format(userId),headers=headers).text
+    relation_text_l=requests.get("https://api.bilibili.com/x/space/acc/info?mid={}&jsonp=jsonp".format(userId),headers=headers).text
+    relation_json=json.loads(relation_text_ff)
+    fans,follows=relation_json['data']['follower'],relation_json['data']['following']
+    relation_json=json.loads(relation_text_l)
+    level=relation_json['data']['level']
+    return fans,follows,level
