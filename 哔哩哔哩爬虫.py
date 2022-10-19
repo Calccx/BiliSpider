af3b8f683d66b28ffc2f08908050c8d818feeeea
Calccx <allcalc@outlook.com>
2022-10-19 23:13:09 +0800

    bugfixed:增加对设置隐私的反馈

--- 哔哩哔哩爬虫.py
+++ 哔哩哔哩爬虫.py
@@ -10,12 +10,12 @@ headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 def getNames(uid):
     url='https://api.bilibili.com/x/relation/followings?vmid='+uid+'&pn=1'
     page_data=requests.get(url=url,headers=headers).text
-    names='"mid":(.*?),".*?"uname":"(.*?)"'
-    
-    names_data=re.findall(names,page_data,re.S)
-    
     follow_json=json.loads(page_data)
+    if follow_json["message"]=="用户已设置隐私，无法查看":
+        return []
     total=follow_json['data']['total']
+    names='"mid":(.*?),".*?"uname":"(.*?)"'
+    names_data=re.findall(names,page_data,re.S)
     if total>250:
         total=250
     pn=math.ceil(total/50)
@@ -32,12 +32,17 @@ def getRelations(userId):
     relation_json=json.loads(relation_text_l)
     level=relation_json['data']['level']
     return fans,follows,level
-merged=list(set(getNames(uid1)[0]).intersection(set(getNames(uid2)[0])))
-fans=[]
-names_data=merged
-total=len(merged)
-for i in range(total):
-    fans.append(getRelations(names_data[i][0]))
-data=np.c_[names_data,fans]
-for name in data:
-    print(name)
+
+if getNames(uid1)==[] or getNames(uid2)==[]:
+    print("用户已设置隐私，无法查看")
+else:
+    merged=list(set(getNames(uid1)[0]).intersection(set(getNames(uid2)[0])))
+    fans=[]
+    names_data=merged
+    total=len(merged)
+    for i in range(total):
+        fans.append(getRelations(names_data[i][0]))
+    data=np.c_[names_data,fans]
+    print("uid           名称     粉丝数 关注数 等级")
+    for name in data:
+        print(name)
